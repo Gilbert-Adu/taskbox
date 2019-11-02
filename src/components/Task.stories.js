edee64b9194a1bdc7ef1e96766f050b7eba7ada5
Gilbert Adu <gea33@cornell.edu>
2019-11-02 05:16:51 -0400

    taskbox UI

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,6 +1,6 @@
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs";
 import Task from './Task';
 
 export const task = {
@@ -15,28 +15,45 @@ export const methods = {
   onArchiveTask: action('onArchiveTask'),
 };
 
-storiesOf('Task', module)
-  .add('default', () => {
+const longTitle = `This task's name is absurdly large. `
+
+storiesOf("Task", module)
+  .addDecorator(withKnobs)
+  .add("default", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task }),
-      methods,
+      props: {
+        task: {
+          type: Object,
+          default: object("task", { ...task })
+        }
+      },
+      methods
     };
   })
-  .add('pinned', () => {
+  .add("pinned", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task: { ...task, state: 'TASK_PINNED' } }),
-      methods,
+      data: () => ({ task: { ...task, state: "TASK_PINNED" } }),
+      methods
     };
   })
-  .add('archived', () => {
+  .add("archived", () => {
     return {
       components: { Task },
       template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
-      data: () => ({ task: { ...task, state: 'TASK_ARCHIVED' } }),
-      methods,
+      data: () => ({ task: { ...task, state: "TASK_ARCHIVED" } }),
+      methods
+    };
+  })
+
+  .add("longTitle", () => {
+    return {
+      components: { Task },
+      template: `<task :task="task" @archiveTask="onArchiveTask" @pinTask="onPinTask"/>`,
+      data: () => ({ task: { ...task, title: longTitle } }),
+      methods
     };
   });
